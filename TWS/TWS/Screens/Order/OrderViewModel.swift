83905aaa809656a842448f07eb14a39b38295d65
xbtamsing <54383855+xbtamsing@users.noreply.github.com>
2021-05-06 19:32:39 -0700

    added methods to trigger rpa and download quote

--- TWS/TWS/Screens/Order/OrderViewModel.swift
+++ TWS/TWS/Screens/Order/OrderViewModel.swift
@@ -8,5 +8,22 @@
 import SwiftUI
 
 class OrderViewModel: ObservableObject {
+    // MARK: - Properties
+    @Published var isLoading: Bool = false
     
+    // MARK: - Methods
+    public func getQuote(for windows: [Window]) {
+        withAnimation { isLoading = true }
+        AuthManager.shared.requestAuth { accessToken in
+            NetworkManager.shared.triggerRPA(for: windows, using: accessToken) {
+                DispatchQueue.main.async {
+                    withAnimation { self.isLoading = false }
+                }
+            }
+        }
+    }
+    
+    public func downloadQuote(using manager: DownloadManager) {
+        manager.downloadQuote()
+    }
 }
