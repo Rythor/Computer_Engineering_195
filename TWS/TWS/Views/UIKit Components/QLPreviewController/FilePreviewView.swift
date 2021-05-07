8f7635dec280b5b3609a18f76afa5cbe4700ef80
xbtamsing <54383855+xbtamsing@users.noreply.github.com>
2021-05-06 19:33:53 -0700

    added representable for qlpreviewcontroller

--- TWS/TWS/Views/UIKit Components/QLPreviewController/FilePreviewView.swift
+++ TWS/TWS/Views/UIKit Components/QLPreviewController/FilePreviewView.swift
@@ -6,15 +6,54 @@
 //
 
 import SwiftUI
+import QuickLook
 
-struct FilePreviewView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct FilePreviewView: UIViewControllerRepresentable {
+    // MARK: - Properties
+    var filePath: String
+    @Binding var isPreviewing: Bool
+    
+    
+    // MARK: - UIViewControllerRepresentable Methods
+    
+    func makeUIViewController(context: Context) -> UINavigationController {
+        let previewController           = QLPreviewController()
+        previewController.delegate      = context.coordinator
+        previewController.dataSource    = context.coordinator
+        
+        let navController                       = UINavigationController(rootViewController: previewController)
+        navController.navigationBar.tintColor   = UIColor.twsBlue
+        
+        return navController
     }
-}
-
-struct FilePreviewView_Previews: PreviewProvider {
-    static var previews: some View {
-        FilePreviewView()
+    
+    func updateUIViewController(_ uiViewController: UINavigationController, context: Context) {
+        // ...
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(filePreviewView: self)
+    }
+    
+    // MARK: - Coordinator
+    
+    final class Coordinator: NSObject, QLPreviewControllerDataSource, QLPreviewControllerDelegate {
+        private let filePreviewView: FilePreviewView
+        
+        init(filePreviewView: FilePreviewView) {
+            self.filePreviewView = filePreviewView
+        }
+        
+        func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
+            return 1
+        }
+        
+        func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
+            return NSURL(fileURLWithPath: filePreviewView.filePath) as QLPreviewItem
+        }
+        
+        func previewControllerDidDismiss(_ controller: QLPreviewController) {
+            filePreviewView.isPreviewing = false
+        }
     }
 }
